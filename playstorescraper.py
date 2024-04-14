36daf79cfb9af2c74ee865a293cad5da11d286db
Aat <karelseaat@gmail.com>
2024-04-14 18:42:48 +0200

    remove newappurl when now found at scrape and set the app also to notinstore anymore or something

--- playstorescraper.py
+++ playstorescraper.py
@@ -140,6 +140,11 @@ while True:
             results.append((result, crawl))
             print(f"{crawl.appid} = Good")
         else:
+
+            playstoreapp = session.query(Playstoreapp).filter(Playstoreapp.appid == crawl.appid).first()
+            if playstoreapp:
+                playstoreapp.removedfromstore = True
+            session.delete(crawl)
             print(f"{crawl.appid} = No good !")
         time.sleep(1)
 
